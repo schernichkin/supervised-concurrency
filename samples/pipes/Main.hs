5f804d7a58d9fdc6ba30fff01915d7f22411d549
Stanislav Chernichkin <schernichkin@gmail.com>
2015-03-04 05:02:38 -0800

    MonadSupervisor

--- samples/pipes/Main.hs
+++ samples/pipes/Main.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE FlexibleContexts      #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE RankNTypes            #-}
+{-# LANGUAGE NoMonomorphismRestriction            #-}
 
 module Main where
 
@@ -13,11 +14,17 @@ import           Control.Monad.Trans.Maybe
 import           Pipes
 import qualified Pipes.Concurrent              as Pipes
 
-newChannel :: (MonadBaseControl IO m) => Pipes.Buffer a -> SupervisedT s m (Sender s (MaybeT IO) a, Receiver s (MaybeT IO) a)
+a = registerChannel undefined
+
+b = registerChannel1 undefined
+
+c = runSupervisedT b
+
+newChannel :: (MonadBaseControl IO m) => Pipes.Buffer a -> SupervisorT s m (Sender s (MaybeT IO) a, Receiver s (MaybeT IO) a)
 newChannel buffer = fmap simplify (newChannel' buffer)
     where simplify (sender, reciever, _) = (sender, reciever)
 
-newChannel' :: (MonadBaseControl IO m) => Pipes.Buffer a -> SupervisedT s m (Sender s (MaybeT IO) a, Receiver s (MaybeT IO) a, IO ())
+newChannel' :: (MonadBaseControl IO m) => Pipes.Buffer a -> SupervisorT s m (Sender s (MaybeT IO) a, Receiver s (MaybeT IO) a, IO ())
 newChannel' buffer = do
     (o, i, s) <- liftBase $ Pipes.spawn' buffer
 
@@ -33,16 +40,16 @@ newChannel' buffer = do
     Channel registeredSender registeredReciever <- registerChannel $ Channel sender reciever
     return (registeredSender, registeredReciever, sealer)
 
-send :: (MonadBase IO m) => Sender s (MaybeT IO) a -> a -> SupervisedT s m Bool
+send :: (MonadBase IO m) => Sender s (MaybeT IO) a -> a -> SupervisorT s m Bool
 send sender = liftM (maybe False $ const True) . liftBase . runMaybeT . sender
 
-recv :: (MonadBase IO m) => Receiver s (MaybeT IO) a -> SupervisedT s m (Maybe a)
+recv :: (MonadBase IO m) => Receiver s (MaybeT IO) a -> SupervisorT s m (Maybe a)
 recv reciever = liftBase $ runMaybeT reciever
 
-seal :: (MonadBase IO m) => IO () -> SupervisedT s m ()
+seal :: (MonadBase IO m) => IO () -> SupervisorT s m ()
 seal = liftBase
 
-fromInput :: (MonadBase IO m) => Receiver s (MaybeT IO) a -> Producer' a (SupervisedT s m) ()
+fromInput :: (MonadBase IO m) => Receiver s (MaybeT IO) a -> Producer' a (SupervisorT s m) ()
 fromInput reciever = loop
     where
         loop = do
@@ -53,7 +60,7 @@ fromInput reciever = loop
                     yield a
                     loop
 
-toOutput :: (MonadBase IO m) => Sender s (MaybeT IO) a -> Consumer' a (SupervisedT s m) ()
+toOutput :: (MonadBase IO m) => Sender s (MaybeT IO) a -> Consumer' a (SupervisorT s m) ()
 toOutput sender = loop
   where
     loop = do
@@ -61,7 +68,7 @@ toOutput sender = loop
         alive <- lift $ send sender a
         when alive loop
 
-consumer :: (MonadIO m) => Consumer Int (SupervisedT s m) ()
+consumer :: (MonadIO m) => Consumer Int m ()
 consumer = do
     liftIO $ threadDelay 100000
     liftIO $ putStrLn $ "consumer started"
@@ -71,7 +78,7 @@ consumer = do
             putStrLn $ "Got message " ++ (show a)
             threadDelay 200000
 
-producer :: (MonadIO m) => Producer Int (SupervisedT s m) ()
+producer :: (MonadIO m) => Producer Int m ()
 producer = do
     liftIO $ threadDelay 200000
     liftIO $ putStrLn $ "producer started"
@@ -80,7 +87,7 @@ producer = do
         yield a
         liftIO $ threadDelay 100000
 
-tracer  :: (MonadBaseControl IO m) => ThreadId -> ThreadState -> SupervisedT s m ()
+tracer  :: (MonadSupervisor m) => ThreadId -> ThreadState -> m ()
 tracer threadId prevState = do
     maybeInfo <- waitTill $ ThreadState threadId (\info -> if _threadState info /= prevState then Just info else Nothing)
     case maybeInfo of
